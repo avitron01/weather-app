cd6562968818843966858b390b1c41479b0017e8
avitron01 <avinapaner@gmail.com>
2021-03-25 11:10:44 +0530

    Added new attributes to models

--- Weather/Weather/Models/WeatherData.swift
+++ Weather/Weather/Models/WeatherData.swift
@@ -6,14 +6,52 @@
 //
 
 import Foundation
+import RealmSwift
 
-struct WeatherData: Codable {
+class WeatherData: Codable {
+    var id: Int //City ID
+    var name: String //City name
     var weather: [Weather]
+    var coordinate: Coordinate
+    var main: Main
+    var visibility: Int
     
-    struct Weather: Codable {
-        var id: Int
-        var main: String
-        var description: String
-        var icon: String
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case weather
+        case coordinate = "coord"
+        case main
+        case visibility
+    }
+}
+
+class Weather: Codable {
+    var id: Int
+    var main: String
+    var description: String
+    var icon: String
+}
+
+struct Coordinate: Codable {
+    var lon: Float
+    var lat: Float
+}
+
+struct Main: Codable {
+    var temp: Float
+    var feelsLike: Float
+    var tempMin: Float
+    var tempMax: Float
+    var pressure: Float
+    var humidity: Float
+
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case pressure
+        case humidity
     }
 }
