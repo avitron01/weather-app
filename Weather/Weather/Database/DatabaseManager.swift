e7e3dfbfbd56e973d69825ae67d9d1f5eca28778
avitron01 <avinapaner@gmail.com>
2021-03-26 00:58:33 +0530

    Added RealmDB config and helper methods

--- Weather/Weather/Database/DatabaseManager.swift
+++ Weather/Weather/Database/DatabaseManager.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import RealmSwift
+
+class DatabaseManager {
+    static let shared = DatabaseManager()
+    let localRealm: Realm
+    
+    init() {
+        localRealm = try! Realm()
+    }
+    
+    func fetchValues<Type: Object>(of type: Type.Type) -> Results<Type> {
+        localRealm.objects(type)
+    }
+    
+    func saveValue<T: Object>(_ object: T) {
+        try! localRealm.write {
+            localRealm.add(object, update: .modified)
+        }
+    }
+}
