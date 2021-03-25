af88ea173b53e1b511ab53cbf9fbf012680e3b78
avitron01 <avinapaner@gmail.com>
2021-03-26 01:03:38 +0530

    Weather service now has the ability to cache responses and added additional helper methods

--- Weather/Weather/Service/WeatherService.swift
+++ Weather/Weather/Service/WeatherService.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import RealmSwift
 
 typealias CurrentWeatherServiceCompletion = (Result<WeatherData>) -> ()
 
@@ -18,9 +19,32 @@ class WeatherService: WeatherServiceProtcol {
         return WeatherNetworkManager.shared
     }()
     
+    private static var database: DatabaseManager = {
+        return DatabaseManager.shared
+    }()
+    
     static func fetchCurrentWeather(for location: String, completion: @escaping CurrentWeatherServiceCompletion) {
         network.fetchData(for: .current(location), type: WeatherData.self) { result in
+            performDBUpdates(for: result)
             completion(result)
         }
+    }
+    
+    private static func performDBUpdates(for weatherDataResult: Result<WeatherData>) {
+        switch weatherDataResult {
+        case .success(let weatherData):
+            cacheWeatherData(for: weatherData.realmData)
+            retrieveCachedData(for: RealmWeatherData.self)
+        case .error(_):
+            break
+        }
+    }
+    
+    private static func cacheWeatherData<T: Object>(for item: T) {
+        database.saveValue(item)
+    }
+    
+    private static func retrieveCachedData<T: Object>(for type: T.Type) -> Results<T> {
+        return database.fetchValues(of: type)
     }
 }
