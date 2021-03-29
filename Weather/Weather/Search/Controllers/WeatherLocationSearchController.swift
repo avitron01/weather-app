ac37aaad4e4b37f14e849a463ea63a3538a5824a
avitron01 <avinapaner@gmail.com>
2021-03-29 12:15:42 +0530

    Added a cancel button in search bar

--- Weather/Weather/Search/Controllers/WeatherLocationSearchController.swift
+++ Weather/Weather/Search/Controllers/WeatherLocationSearchController.swift
@@ -8,18 +8,20 @@
 import UIKit
 
 class WeatherLocationSearchController: BaseViewController {
+    //MARK: - Outlet properties
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchLabel: UILabel!
     @IBOutlet weak var searchBar: UISearchBar!
     
+    //MARK: - Private properties
     private let stringConstants = StringConstants.WeatherLocationSearchController.self
     private let cellConstants = TableCellConstants.WeatherLocationSearchController.self
-    
     private let viewModel = WeatherLocationSearchViewModel()
     private var shouldHideRecentsList: Bool {
         return viewModel.recentlySearched.value.count <= 0
     }
     
+    //MARK: - View controller life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         self.viewModel.recentlySearched.bind { (weatherData) in
@@ -38,25 +40,27 @@ class WeatherLocationSearchController: BaseViewController {
         }
     }
     
-    func showErrorAlert(with message: String) {
-        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
-        alert.addAction(okAction)
-        self.parent?.present(alert, animated: true, completion: nil)
-    }
-    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.viewModel.updateRecentlySearched()
         self.hideRecentsList(self.shouldHideRecentsList)
     }
     
+    //MARK: - Helper methods
+    func showErrorAlert(with message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(okAction)
+        self.parent?.present(alert, animated: true, completion: nil)
+    }
+
     func hideRecentsList(_ hide: Bool) {
         self.tableView.isHidden = hide
         self.searchLabel.isHidden = !hide
     }
 }
 
+//MARK: - Table view data source and delegates
 extension WeatherLocationSearchController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return stringConstants.recentlySearched
@@ -79,13 +83,28 @@ extension WeatherLocationSearchController: UITableViewDelegate, UITableViewDataS
     }
 }
 
+//MARK: - Search delegate
 extension WeatherLocationSearchController: UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBar.setShowsCancelButton(true, animated: true)
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        searchBar.setShowsCancelButton(false, animated: true)
+    }
+    
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         if let searchString = searchBar.text, !searchString.isEmpty {
             viewModel.fetchWeatherData(for: searchString, context: self)
-            clearSearchBar()
-            dismisKeyboard()
+            self.clearSearchBar()
+            self.dismisKeyboard()
         }
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.setShowsCancelButton(false, animated: true)
+        self.clearSearchBar()
+        self.dismisKeyboard()
     }
     
     func clearSearchBar() {
